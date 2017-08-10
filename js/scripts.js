a0099c0a3eb1dbc3abd8ed0cb16f714db8654cf1
Austin Cummings and Sean Slingerland <austin@>
2017-08-10 13:48:19 -0700

    html/css alterations

--- js/scripts.js
+++ js/scripts.js
@@ -1,10 +1,23 @@
 $(document).ready(function() {
   var height = parseInt(prompt("How tall are you? In inches"));
 
-  if (height > 21) {
+  if (height >= 45) {
     $('.all-rides').show();
   }
-  // else {
-  //   $('little-rides').show();
-  // }
+  else {
+    $('.little-rides').show();
+  }
 });
+
+$(function() {
+  $("button#all-rides-highlighted").click(function() {
+    $(".all-rides").show();
+    $(".little-rides").addClass("green-background");
+    $("#big-rides").addClass("red-background");
+})
+  })
+
+// (btn-primary).onClick() {
+//   $('.all-rides').show();
+//   $('.little-rides').color(green);
+// }
